03fcc84deeaa21329b958e02c93b1fc2d0b24f3d
jacklovett <jacklovett@mail.com>
2024-10-16 18:16:32 +1000

    Add customer model

---
+++ python_auctioneer/models/customer.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String
+from python_auctioneer.models import Base
+
+
+class Customer(Base):
+    __tablename__ = "customers"
+
+    customer_id = Column(Integer, primary_key=True, autoincrement=True)
+    customer_name = Column(String, nullable=False)
+    customer_address = Column(String, nullable=False)
