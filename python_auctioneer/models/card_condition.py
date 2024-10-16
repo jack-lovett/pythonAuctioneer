105ddb3c783f5f1ab2b335b594cf54c46d677c48
jacklovett <jacklovett@mail.com>
2024-10-16 18:00:39 +1000

    Add card condition model

---
+++ python_auctioneer/models/card_condition.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String, Float
+from python_auctioneer.models.base import Base
+
+
+class CardCondition(Base):
+    __tablename__ = "card_conditions"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    condition_name = Column(String, unique=True, nullable=False)
+    condition_discount_percentage = Column(Float)
