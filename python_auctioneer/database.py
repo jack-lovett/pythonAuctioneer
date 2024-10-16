c4299aaa9e304a7b095afda2d8e35b5dc0680278
jacklovett <jacklovett@mail.com>
2024-10-16 17:53:52 +1000

    Database manager rework to use SQLAlchemy

---
+++ python_auctioneer/database.py
@@ -0,0 +1,31 @@
+"""Database manager."""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from python_auctioneer.config import DATABASE_URL
+from python_auctioneer.models import Base
+
+# Create the SQLAlchemy engine
+engine = create_engine(DATABASE_URL)
+
+# Create a configured "Session" class
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+# Dependency to get a SQLAlchemy session
+def get_db():
+    database = SessionLocal()
+    try:
+        yield database
+        database.commit()
+    except Exception:
+        database.rollback()
+        raise
+    finally:
+        database.close()
+
+
+# Initialise the database
+def init_db():
+    # from python_auctioneer.models import Card
+    Base.metadata.create_all(bind=engine)
