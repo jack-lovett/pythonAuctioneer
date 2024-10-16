d71cee55caae9c5bb69cb119fbf29a58bdb347e8
jacklovett <jacklovett@mail.com>
2024-10-16 18:09:38 +1000

    Fix inconsistent naming in card condition columns

--- python_auctioneer/models/card_condition.py
+++ python_auctioneer/models/card_condition.py
@@ -5,6 +5,6 @@ from python_auctioneer.models import Base
 class CardCondition(Base):
     __tablename__ = "card_conditions"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    condition_id = Column(Integer, primary_key=True, autoincrement=True)
     condition_name = Column(String, unique=True, nullable=False)
     condition_discount_percentage = Column(Float)
