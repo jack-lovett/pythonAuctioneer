20ed07568b83d3894fb97fc1de796f474d08f09b
jacklovett <jacklovett@mail.com>
2024-10-17 23:40:42 +1000

    Rename get_card_condition function to get_card_condition_service

--- python_auctioneer/services/card_condition.py
+++ python_auctioneer/services/card_condition.py
@@ -1,4 +1,4 @@
 from python_auctioneer.models.card_condition import CardCondition
 
-def get_card_condition(database, condition_id):
+def get_card_condition_service(database, condition_id):
     return database.query(CardCondition).filter(CardCondition.condition_id == condition_id).first()
