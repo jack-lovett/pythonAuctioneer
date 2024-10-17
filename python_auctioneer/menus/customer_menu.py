44d8c245b4a7960fbde9cd61d8236d00e90fc53b
jacklovett <jacklovett@mail.com>
2024-10-17 18:26:09 +1000

    Add create customer function

--- python_auctioneer/menus/customer_menu.py
+++ python_auctioneer/menus/customer_menu.py
@@ -1,5 +1,9 @@
 """Manage customers menu."""
 
+from python_auctioneer.database import SessionLocal
+from python_auctioneer.services.customer import create_customer_service, get_customer_service
+from tabulate import tabulate
+
 MENU = """Customer Management
 1. Create customer
 2. View all customers
@@ -10,12 +14,12 @@ MENU = """Customer Management
 
 def customer_menu():
     """Customer menu."""
+    database = SessionLocal()
     print(MENU)
     choice = input(">> ")
     while choice != "5":
         if choice == "1":
-            # create_customer()
-            pass
+            create_customer(database)
         elif choice == "2":
             # view_customers()
             pass
@@ -29,3 +33,20 @@ def customer_menu():
             print("Invalid choice.")
         print(MENU)
         choice = input(">> ")
+
+
+def create_customer(database):
+    try:
+        customer_firstname = input("Enter the customer first name: ")
+        customer_lastname = input("Enter the customer last name: ")
+        customer_address = input("Enter the customer address: ")
+
+        customer_data = {
+            "customer_firstname": customer_firstname,
+            "customer_lastname": customer_lastname,
+            "customer_address": customer_address,
+        }
+
+        create_customer_service(database, customer_data)
+    except ValueError as e:
+        print(f"Error: {e}")
