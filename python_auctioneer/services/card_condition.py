b230e19706623edbaac0f9a90b61bc987df609f1
jacklovett <jacklovett@mail.com>
2024-10-17 23:38:07 +1000

    Add get card condition function to service file

---
+++ python_auctioneer/services/card_condition.py
@@ -0,0 +1,4 @@
+from python_auctioneer.models.card_condition import CardCondition
+
+def get_card_condition(database, condition_id):
+    return database.query(CardCondition).filter(CardCondition.condition_id == condition_id).first()
