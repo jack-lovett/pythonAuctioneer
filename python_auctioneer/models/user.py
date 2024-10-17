9d95d18b0f0d156af711b176541b8e2532657ffc
jacklovett <jacklovett@mail.com>
2024-10-17 22:50:10 +1000

    Simplify imports in user model
    Removed unnecessary imports from sqlalchemy and a redundant relative import. This change helps in maintaining cleaner and more readable code by only including necessary components.

--- python_auctioneer/models/user.py
+++ python_auctioneer/models/user.py
@@ -1,6 +1,5 @@
-from sqlalchemy import Column, Integer, String, Text, DateTime, CheckConstraint
-from sqlalchemy.orm import relationship
-from python_auctioneer.models import Base
+from sqlalchemy import Column, Integer, String
+from . import Base
 
 class User(Base):
     __tablename__ = "users"
