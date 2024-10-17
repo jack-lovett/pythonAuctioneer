e93d43761c7310bfc2ce287cd33200b45a3f1884
jacklovett <jacklovett@mail.com>
2024-10-17 17:19:34 +1000

    Add create auction functionality to auction menu

--- python_auctioneer/menus/auction_menu.py
+++ python_auctioneer/menus/auction_menu.py
@@ -1,5 +1,9 @@
 """Manage auctions menu."""
 
+import datetime
+from python_auctioneer.database import SessionLocal
+from python_auctioneer.services.auction import create_auction_service
+
 MENU = """Auction Management
 1. Create auction
 2. View all auctions
@@ -10,12 +14,12 @@ MENU = """Auction Management
 
 def auction_menu():
     """Auction menu."""
+    database = SessionLocal()
     print(MENU)
     choice = input(">> ")
     while choice != "5":
         if choice == "1":
-            # create_auction()
-            pass
+            create_auction(database)
         elif choice == "2":
             # view_auctions()
             pass
@@ -29,3 +33,25 @@ def auction_menu():
             print("Invalid choice.")
         print(MENU)
         choice = input(">> ")
+
+
+def create_auction(database):
+    try:
+        auction_description = input("Enter the auction description: ")
+        auction_open_time_str = input("Enter the auction start date DD/MM/YYYY: ")
+        auction_close_time_str = input("Enter the auction end date DD/MM/YYYY: ")
+
+        # Convert input strings to datetime objects
+        auction_open_time = datetime.datetime.strptime(auction_open_time_str, '%d/%m/%Y')
+        auction_close_time = datetime.datetime.strptime(auction_close_time_str, '%d/%m/%Y')
+
+        auction_data = {
+            "auction_description": auction_description,
+            "auction_open_time": auction_open_time,
+            "auction_close_time": auction_close_time,
+        }
+
+        create_auction_service(database, auction_data)
+    except ValueError as e:
+        print(f"Error: {e}")
+        print("Invalid date format. Please use DD/MM/YYYY.")
