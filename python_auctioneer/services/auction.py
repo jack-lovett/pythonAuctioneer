b5341bb9d9534e82f2e721513293c7eaf8a90cfe
jacklovett <jacklovett@mail.com>
2024-10-31 10:26:02 +1000

    Refactor auction services to utilize CRUD operations

--- python_auctioneer/services/auction.py
+++ python_auctioneer/services/auction.py
@@ -1,15 +1,14 @@
 from sqlalchemy.exc import IntegrityError, SQLAlchemyError
+from python_auctioneer.crud.factory import CRUDAuction
 from python_auctioneer.models.auction import Auction
+
+crud_auction = CRUDAuction()
 
 
 def create_auction_service(database, auction_data):
     """Create new auction."""
     try:
-        new_auction = Auction(**auction_data)
-        database.add(new_auction)
-        database.commit()
-        database.refresh(new_auction)
-        return new_auction
+        return crud_auction.create(database, auction_data)
     except IntegrityError as e:
         database.rollback()
         raise ValueError(f"Error creating auction: {e}")
@@ -18,25 +17,19 @@ def create_auction_service(database, auction_data):
 def get_auctions_service(database):
     """View all auctions."""
     try:
-        return database.query(Auction).all()
+        return crud_auction.get_all(database)
     except SQLAlchemyError as e:
         print(f"Error viewing auctions: {e}")
         return []
 
 
-def update_auction_service(database, auction_id, description, open_time, close_time):
+def update_auction_service(database, auction_id, auction_data):
     """Update an existing auction."""
     try:
-        auction = database.query(Auction).filter(Auction.auction_id == auction_id).first()
-        if auction:
-            auction.auction_description = description
-            auction.auction_open_time = open_time
-            auction.auction_close_time = close_time
-            database.commit()
-            database.refresh(auction)
-            return auction
-        else:
-            raise ValueError("Auction not found.")
+        auction = crud_auction.get(database, auction_id)
+        if not auction:
+            raise ValueError(f"Auction {auction_id} does not exist.")
+        return crud_auction.update(database, auction_id, auction_data)
     except IntegrityError as e:
         database.rollback()
         raise ValueError(f"Error updating auction: {e}")
@@ -50,13 +43,10 @@ def get_auction_by_id(database, auction_id):
 def delete_auction_service(database, auction_id):
     """Delete an auction by its ID."""
     try:
-        auction = database.query(Auction).filter(Auction.auction_id == auction_id).first()
-        if auction:
-            database.delete(auction)
-            database.commit()
-            return True
-        else:
-            return False
+        auction = crud_auction.get(database, auction_id)
+        if not auction:
+            raise ValueError(f"Auction {auction_id} does not exist.")
+        return crud_auction.delete(database, auction_id)
     except IntegrityError as e:
         database.rollback()
         raise ValueError(f"Error deleting auction: {e}")
